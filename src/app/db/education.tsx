989bb2b31b4b5c896ca86ccb41c251e61c69745a
Mahmoud Zakaria <manuscrit974@gmail.com>
2024-12-11 09:45:32 +0100

    en cours

--- src/app/db/education.tsx
+++ src/app/db/education.tsx
@@ -3,12 +3,36 @@ export type Education = {
   title: string;
   organization: string;
   description: string[] | undefined;
+  logo?: string; // Ajout du champ pour le logo
 };
 
 export const education: Education[] = [
   {
+    id: 5,
+    title: "AWS Certified Developer - Associate",
+    organization: "Amazon Web Services (AWS)",
+    description: [
+      "- Développer des applications cloud sur AWS",
+      "- Utiliser les services AWS pour implémenter des solutions serverless",
+      "- Apprendre à dépanner et à surveiller des applications sur AWS",
+    ],
+    logo: "/images/aws-logo.png", // Lien vers le logo AWS Developer Associate
+  },
+  {
+    id: 4,
+    title: "AWS Certified Cloud Practitioner",
+    organization: "Amazon Web Services (AWS)",
+    description: [
+      "- Comprendre les services cloud AWS et les concepts associés",
+      "- Gérer les ressources AWS et les solutions basées sur AWS",
+      "- Apprendre à sécuriser et à conformer les environnements cloud sur AWS",
+    ],
+    logo: "/images/aws-logo.png", // Lien vers le logo AWS Cloud Practitioner
+  },
+
+  {
     id: 1,
-    title: "Développeur concepteur logiciel, RNCP niveau VI  (bac +3/4)",
+    title: "Développeur concepteur logiciel, RNCP niveau VI (bac +3/4)",
     organization: "OpenClassrooms",
     description: [
       "- Concevoir et développer des interfaces utilisateurs avec React",
@@ -17,10 +41,11 @@ export const education: Education[] = [
       "- Intégrer des données provenant de diverses sources back-end et bases de données",
       "- Assurer la maintenance et la mise à jour des applications existantes",
     ],
+    logo: "/images/ocs.png", // Lien vers le logo OpenClassrooms
   },
   {
     id: 2,
-    title: "Développeur informatique, RNCP niveau III  (bac +2)",
+    title: "Développeur informatique, RNCP niveau III (bac +2)",
     organization: "OpenClassrooms",
     description: [
       "- Contribuer à un site web responsive et dynamique graduellement tout au long du projet, de l’intégration des éléments des maquettes graphiques (développement front-end avec HTML, CSS, JavaScript et React)",
@@ -28,6 +53,7 @@ export const education: Education[] = [
       "- Optimiser les performances et l’accessibilité de sites web existants pour permettre leur fonctionnement optimal et une bonne visibilité sur les moteurs de recherche (SEO)",
       "- Gérer un projet web de A à Z : de la planification du projet à la présentation de la solution au client, en passant par la rédaction des spécifications techniques",
     ],
+    logo: "/images/ocs.png", // Lien vers le logo OpenClassrooms
   },
   {
     id: 3,
@@ -40,5 +66,6 @@ export const education: Education[] = [
       "- Améliorer le référencement d'un site web",
       "- Assurer la compatibilité avec les différents navigateurs du marché ainsi que les appareils mobiles",
     ],
+    logo: "/images/ocs.png", // Lien vers le logo OpenClassrooms
   },
 ];
